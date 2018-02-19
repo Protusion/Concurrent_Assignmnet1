b6b02cabe69d972dcdd5583308f5db003119de76
Alberto <Alberto@172.19.186.182>
2018-02-19 09:39:18 +0100

    First part is now done Second part almost done

---
+++ Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Concurrent_Assignmnet1;
+
+import Concurrent_Assignmnet1.A.*;
+import Concurrent_Assignmnet1.B.*;
+
+/**
+ *
+ * @author Alberto
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        //First exercise (A)
+        
+        //Reference reference = new Reference();
+        //reference.thread();
+        
+        //Second excercise (B)
+        
+        Numbers numbers = new Numbers();
+        
+    }
+    
+}
