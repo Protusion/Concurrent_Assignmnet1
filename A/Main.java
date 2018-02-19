187a7a4cb376e28be2a7ed63e52346fe7a190aeb
Alberto <Alberto@172.19.186.182>
2018-02-19 09:11:41 +0100

    First part is now done

---
+++ A/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Concurrent_Assignmnet1.A;
+
+/**
+ *
+ * @author Alberto
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        Reference reference = new Reference();
+        reference.thread();
+    }
+    
+}
--- A/Reference.java
+++ A/Reference.java
@@ -1,8 +1,21 @@
-package concurrent_assignment1.A;
+package Concurrent_Assignmnet1.A;
 
 /**
- * Get the reference of the main thread */
-
+ * Get the reference of the main thread
+ */
 public class Reference {
 
+    public static void thread() {
+        try {
+            Thread h = Thread.currentThread();
+            System.out.println(h.getName());
+            h.setName("much slower main :)");
+            for (int i = 1; i < 100; i = i + 2) {
+                System.out.println(i + " " + h.getName());
+                Thread.sleep(2000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("A wild Interruped Exception appared");
+        }
+    }
 }
