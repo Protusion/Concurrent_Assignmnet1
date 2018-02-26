402568aa5da95990c59b5e9f7c59e3c1d6b20f38
Alberto <Alberto@172.19.186.182>
2018-02-26 19:23:40 +0100

    Part B finished

--- B/Numbers.java
+++ B/Numbers.java
@@ -4,21 +4,18 @@ package Concurrent_Assignmnet1.B;
  * Create class Numbers which instantiates 5 T_PrintID objects (with ID 1 to 5) and runs them.
  * Can you detect some problem? Can you explain the reason?
  * 
- * @author 
- *
+ * @author Rocío y Alberto
+     *
  */
 
 public class Numbers {
-    public Numbers(){
+ 
+    
+    public static void main (String[] Args){
         T_PrintID ids[] = new T_PrintID[5];
         for(int i = 1; i < 5; i++){
             ids[i] = new T_PrintID(i);
             ids[i].start();
         }
-        
-    }
-    
-    public static void main (String[] Args){
-        Numbers numbers = new Numbers();
     }
 }
